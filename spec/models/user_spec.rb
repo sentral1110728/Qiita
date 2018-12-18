539b6ca7888e7df68734205622deda122353895d
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2018-12-18 16:48:02 +0900

    rspec nicknameが空値の場合登録できない

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+
     it "nickname, email, password, password_confirmationが存在と登録できる" do
       user = build(:user)
       expect(user).to be_valid
     end
+
+    it "nicknameが空値の場合登録できない" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
   end
 end
