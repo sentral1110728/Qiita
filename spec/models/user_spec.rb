6bdf83cdb0d04213960caeca349a071eaedd7120
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2018-12-18 17:14:32 +0900

    rspec passwordが存在してもpassword_confirmationが空では登録できない

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,11 @@ describe User do
       expect(user.errors[:password]).to include("can't be blank")
     end
 
+    it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
   end
 end
