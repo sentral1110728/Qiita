7521c92e7a4e0d2ea405760f810adb26371f9ca5
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-01-04 17:16:54 +0900

    環境別 設定ファイル編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '13.115.193.245', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
