1f2a408e7a1e766cb94bfe6619771313fd581f9a
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-01-07 12:19:06 +0900

    Sshキー　ディレクトリ指定変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/okamurayuu/suguru0203.pem']
-
+                  keys: ['~/.ssh/suguru0203.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
