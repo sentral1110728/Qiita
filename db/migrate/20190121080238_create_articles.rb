98f45a566414a535580e996984d93b93b1d7d98c
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-01-21 17:30:06 +0900

    データベースにarticlesテーブルを生成

--- db/migrate/20190121080238_create_articles.rb
+++ db/migrate/20190121080238_create_articles.rb
@@ -1,8 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
-      t.timestamps
+      t.string   :title, null: false, index: true
+      t.text     :text, null: false
+      t.string   :image
+      t.references :user, null: false, foreign_key: true
+      t.timestamps null: true
     end
   end
 end
