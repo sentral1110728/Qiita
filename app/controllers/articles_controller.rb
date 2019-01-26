1933a94c95440cb1d9cd77c883aacea11953c206
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-01-26 14:21:26 +0900

    ストロングパラメーターを使用しarticleを保存する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,12 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    Article.create(article_params)
+  end
+
+  private
+  def article_params
+    params.permit(:title, :text, :image)
   end
 
 end
