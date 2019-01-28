1e6f4d501d236d3f38c7c9c265160e517c47bf77
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-01-28 16:42:19 +0900

    orderメソッド記述方法変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.order("created_at DESC")
+    @articles = Article.order(created_at: :desc)
   end
 
   def new
