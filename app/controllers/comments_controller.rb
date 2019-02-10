aee5de18f3c4c6e850847abf7efab87e7d8d8348
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-02-10 12:16:37 +0900

    @commentにコメントを保存する記述を入れる

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content).merge(article_id: params[:article_id], user_id: current_user.id)
