aa53699e7ac7971190ebb9fdb8a8f68253d11a0e
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-02-10 12:14:52 +0900

    commentsテーブルとやりとりするためストロングパラメーターを設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,6 @@
 class CommentsController < ApplicationController
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(article_id: params[:article_id], user_id: current_user.id)
+  end
 end
