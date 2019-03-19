31ed07ba78738ea21b606acc2d86e1a5674ebe9f
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-03-19 12:39:18 +0900

    自動デプロイするため環境変数を明示的に指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,12 @@ namespace :deploy do
   end
 end
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID2: ENV["AWS_ACCESS_KEY_ID2"],
+  AWS_SECRET_ACCESS_KEY2: ENV["AWS_SECRET_ACCESS_KEY2"]
+}
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
